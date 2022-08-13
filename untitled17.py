7d9e1b083fac17245ad25e8bb5646e3022d1d5e8
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-13 13:47:34 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -176,6 +176,4 @@ if st.button("Run Script"):
      for s in df3_temp.columns:
           df[s] = np.nan
      st.write(df3)
-     for i in range(2):
-          st.write(str(df['Abbreviation_list'].iloc[i]))
-          st.write(df3[df3['Abbreviation_list'] == 2])
+     st.write(df['Abbreviation_list'])
