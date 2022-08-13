2bab367c3fde11e4c3a324d0c8df96d3167d46be
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-13 13:16:12 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -170,4 +170,6 @@ if st.button("Run Script"):
      df3.columns = df3.iloc[1]
      df3 = df3.drop([0,1])
      df3 = df3.rename(columns = {'FIPS': 'Abbreviation_list'})
+     df4 = df3[['Abbreviation']]
      st.write(df3)
+     st.write(df4)
