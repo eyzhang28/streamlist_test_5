5ab35f4544d46c52b646682541358a75df31d604
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-13 22:34:51 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -182,8 +182,7 @@ if st.button("Run Script"):
                output = xa_cleaning(output)
           WEB_ID = output[output.index('WEB ID:') + 8: output.index('WEB ID: ') + 20]
           PSWD = output[output.index('PASSWORD:') + 10: output.index('PASSWORD:') + 18]
-          df4 = df4[df4['Web_ID'].notna()]
-          df5 = df4[df4['Web_ID'].str.contains(WEB_ID)]
-          df6 = df5[df5['Password'].str.contains(PSWD)]
-          st.write(df6)
+          st.write(WEB_ID)
+          st.write(PSWD)
+
           
