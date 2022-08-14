3a292c6e6ec501d6081b545366a1696d10241bee
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-14 13:35:38 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -164,6 +164,7 @@ if st.button("Run Script"):
      df = pd.read_csv(print_data)
      df['Web_ID'] = df['Web_ID'].astype('int')
      df = df.applymap(str)
+     st.write(df2)
      for i in range(len(df)):
           if len(df['Abbreviation_list'].iloc[i]) == 1:
                df['Abbreviation_list'].iloc[i] = '0' + df['Abbreviation_list'].iloc[i]
