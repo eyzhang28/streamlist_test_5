5079118f114d358d33c8a75e7624142b53eb1130
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-14 14:58:32 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -42,6 +42,13 @@ def xa_cleaning(s):
     except:
         pass
     return s
+
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+     
 def compare_dict(df6, proofs_dictionary):
     counter = 0
     if (proofs_dictionary['Form Identification'] == 'BLS 3023 - Industry Verification Form'):
@@ -228,6 +235,7 @@ def compare_dict(df6, proofs_dictionary):
     
 
 if st.button("Run Script"):
+     show_pdf(proofs_data)
      df = pd.read_csv(print_data)
      df['Web_ID'] = df['Web_ID'].astype('int')
      df = df.applymap(str)
