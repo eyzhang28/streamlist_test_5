43f0dc8dbcfc0cfc776714e11fdcb05264fb88f7
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-13 22:25:30 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -177,3 +177,4 @@ if st.button("Run Script"):
      #df4 = pd.merge(df3, df, on = 'BMA_State', how = 'outer')
      #st.write(df4['UI_Number'])
      data = tb.read_pdf(proofs_data, area = (150, 400, 180, 600), pages = 1)
+     st.write(data)
