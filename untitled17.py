4012a340266786830bdb0adb1041b87151a889cd
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-14 12:18:58 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -210,6 +210,8 @@ if st.button("Run Script"):
                else:
                     st.write("Incorrect Notice")
           data = tb.read_pdf(proofs_data, area = (23, 52, 144, 333), pages = i)
+          st.write(data)
+          """
           if (df6['State Agency Name (50 char)'].iloc[0].lower().strip() == data[0].columns[0].lower().strip()):
                proofs_dictionary['State Agency Name'] = data[0].columns[0]
                if (pd.isnull(df6['Return Address Line 2'].iloc[0]) and pd.isnull(df6['Department Name (50 char)'].iloc[0])):
