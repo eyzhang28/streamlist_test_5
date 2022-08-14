902314bcd86082e4cc6271b4c8eb3d25e6435acc
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-13 23:06:16 -0700

    Update untitled17.py

--- untitled17.py
+++ untitled17.py
@@ -176,7 +176,7 @@ if st.button("Run Script"):
      df4 = pd.merge(df3, df, on = 'BMA_State', how = 'outer')
      df4 = df4[df4['Web_ID'].notna()]
      for i in range(len(df4)):
-          while (len(df4['Web_ID'].iloc[i] < 12):
+          while (len(df4['Web_ID'].iloc[i]) < 12):
                  df4['Web_ID'].iloc[i] = '0' + df4['Web_ID'].iloc[i]
      st.write(df4['Web_ID'])
                   
